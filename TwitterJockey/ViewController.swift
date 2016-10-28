435e7ebe7bda6b434ea32719d684f73eb09a5801
Craig Vargas <cvar@Craigs-MacBook-Pro.local>
2016-10-28 14:02:32 -0400

    Initial Commit

---
+++ TwitterJockey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterJockey
+//
+//  Created by Craig Vargas on 10/28/16.
+//  Copyright © 2016 Cvar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
